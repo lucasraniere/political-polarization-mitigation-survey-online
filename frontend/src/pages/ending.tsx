763117b894e1bc2e8983a456e89c7e868366dbc6
Lucas Raniére <lucas.raniere94@gmail.com>
2024-05-16 15:06:42 -0400

    adding initial files for frontend

---
+++ frontend/src/pages/ending.tsx
@@ -0,0 +1,11 @@
+export default function EndingPage() {
+    return (
+        <main>
+            <h1>Muito Obrigado!</h1>
+            <h2>Para finalizar o survey clique no botão abaixo para ser redirecionado
+                de volta ao Prolific.
+            </h2>
+            <button>Finalizar</button>
+        </main>
+    )
+}
