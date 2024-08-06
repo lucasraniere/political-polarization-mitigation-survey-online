a24fcf541e3db634fe47345120ea6271cfcadccb
Lucas Raniere <lucas.raniere94@gmail.com>
2024-08-06 17:48:08 +0000

    updating repo before making changes on the front end

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,7 +6,10 @@ import lib.db_utils as db
 
 os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
-cors = CORS(app, origins='*')
+#cors = CORS(app, origins='*')
+cors = CORS(app, origins=[
+    '150.165.85.100'
+])
 DATA_PATH = '../data/log/'
 
 
