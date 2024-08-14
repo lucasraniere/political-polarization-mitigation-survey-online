3281ccd615ecc3c406266f959f57bef456805d92
Lucas Raniére <lucas.raniere94@gmail.com>
2024-08-14 16:32:06 -0400

    now im adding the stuff I said on the previous commit

--- backend/data/database/survey_db.sql
+++ backend/data/database/survey_db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.4.2, for Linux (x86_64)
 --
 -- Host: localhost    Database: survey_db
 -- ------------------------------------------------------
--- Server version	8.0.39-0ubuntu0.22.04.1
+-- Server version	8.4.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -182,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-14 17:39:29
+-- Dump completed on 2024-08-14 15:45:48
---
+++ backend/src/export_db.py
@@ -0,0 +1,77 @@
+import os
+import mysql.connector
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+os.chdir(os.path.dirname(__file__))
+DATA_DIR = '../data/database/'
+
+load_dotenv()
+engine = create_engine('mysql+mysqlconnector://root:{}@localhost:3306/survey_db'.format(os.getenv('MYSQL_PASSWORD')))
+
+def export_to_sql():
+    print('Generating sql file...')
+    os.system('mysqldump -u root -p{} survey_db > {}survey_db.sql'.format(os.getenv('MYSQL_PASSWORD'), DATA_DIR))
+    print('SQL file generated.')
+
+
+def export_participants():
+    print('Exporting participants...')
+    file_path = os.path.join(DATA_DIR, 'participants.parquet')
+    query = 'SELECT * FROM Participants;'
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    df.to_parquet(file_path, index=False)
+
+
+def export_answers():
+    print('Exporting answers...')
+    file_path = os.path.join(DATA_DIR, 'answers.parquet')
+    query = 'SELECT * FROM Answers;'
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    df.to_parquet(file_path, index=False)
+
+
+def export_tweets():
+    print('Exporting tweets...')
+    file_path = os.path.join(DATA_DIR, 'tweets.parquet')
+    query = 'SELECT * FROM Tweets;'
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    df.to_parquet(file_path, index=False)
+
+
+def export_rephrases():
+    print('Exporting rephrased tweets...')
+    file_path = os.path.join(DATA_DIR, 'rephrases.parquet')
+    query = 'SELECT * FROM RephrasedTweets;'
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    df.to_parquet(file_path, index=False)
+
+
+def export_sessions():
+    print('Exporting sessions...')
+    file_path = os.path.join(DATA_DIR, 'sessions.parquet')
+    query = 'SELECT * FROM Sessions;'
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    df.to_parquet(file_path, index=False)
+
+
+def export_db():
+    export_to_sql()
+    export_participants()
+    export_answers()
+    export_tweets()
+    export_rephrases()
+    export_sessions()
+
+def main():
+    export_db()
+
+
+if __name__ == '__main__':
+    main()
