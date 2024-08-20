c295eb6c04d0770fb9e6126b725c323a08f42ce2
Lucas Raniére <lucas.raniere94@gmail.com>
2024-08-20 18:35:22 -0400

    fixing double quotes bugs

--- frontend/src/components/PolarizationDef.tsx
+++ frontend/src/components/PolarizationDef.tsx
@@ -12,7 +12,7 @@ export default function PolarizationDef() {
                     <li><b>Teorias conspiratórias:</b> Narrativas que propõem explicações alternativas, sem evidências sólidas, para eventos complexos. Costumam envolver a crença em conspirações ocultas de poderosos grupos ou indivíduos;</li>
                     <li><b>Desumanização ou demonização de opositores:</b> Retrato dos opositores políticos como vilões, inimigos ou ameaças à sociedade. Usa linguagem que desumaniza ou demoniza os adversários, diminuindo sua credibilidade e legitimidade;</li>
                     <li><b>Imputação de crime:</b> Atribuição de atos criminosos ou comportamento moralmente questionável a opositores sem evidências substanciais. Usada para desacreditar oponentes ou gerar desconfiança pública;</li>
-                    <li><b>Linguagem divisiva:</b> Comunicação que aumenta as divisões sociais, políticas ou culturais. Usa linguagem que separa as pessoas em grupos opostos, alimentando conflitos e hostilidades, tratando temas como &quot;nós contra eles"&quot;.</li>
+                    <li><b>Linguagem divisiva:</b> Comunicação que aumenta as divisões sociais, políticas ou culturais. Usa linguagem que separa as pessoas em grupos opostos, alimentando conflitos e hostilidades, tratando temas como &quot;nós contra eles&quot;.</li>
                 </ul>
             </div>
         </div>
