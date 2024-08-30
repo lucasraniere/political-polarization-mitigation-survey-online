2d9f71786166342e450580df49f71ec2b75c35c6
Lucas Raniére <lucas.raniere94@gmail.com>
2024-08-30 17:22:31 -0400

    adding the 'i dont know / Ignore' option in the user's political leaning scale

--- frontend/src/components/index/PoliticalLeaning.tsx
+++ frontend/src/components/index/PoliticalLeaning.tsx
@@ -6,40 +6,45 @@ export default function PoliticalLeaning(props: PoliticalLeaningProps) {
     return (
         <div id="political-leaning">
             <span className="scale-value">
-                <label htmlFor="esquerda-radical">Esquerda radical <br />
+                <label htmlFor="esquerda-radical">Esquerda radical<br />
                     <input type="radio" id="esquerda-radical" name="posicionamento" value="esquerda-radical" onChange={() => props.changeHandler(1)} />
                 </label>
             </span>
             <span className="scale-value scale-value-color">
-                <label htmlFor="esquerda">Esquerda <br />
+                <label htmlFor="esquerda">Esquerda<br />
                     <input type="radio" id="esquerda" name="posicionamento" value="esquerda"
                     onChange={() => props.changeHandler(2)} />
                 </label>
             </span>
             <span className="scale-value">
-                <label htmlFor="centro-esquerda">Centro-esquerda <br />
+                <label htmlFor="centro-esquerda">Centro-esquerda<br />
                     <input type="radio" id="centro-esquerda" name="posicionamento" value="centro-esquerda" onChange={() => props.changeHandler(3)} />
                 </label>
             </span>
             <span className="scale-value scale-value-color">
-                <label htmlFor="centro">Centro <br />
+                <label htmlFor="centro">Centro<br />
                     <input type="radio" id="centro" name="posicionamento" value="centro"
                     onChange={() => props.changeHandler(4)} />
                 </label>
             </span>
             <span className="scale-value">
-                <label htmlFor="centro-direita">Centro-direita <br />
+                <label htmlFor="centro-direita">Centro-direita<br />
                     <input type="radio" id="centro-direita" name="posicionamento" value="centro-direita" onChange={() => props.changeHandler(5)} />
                 </label>
             </span>
             <span className="scale-value scale-value-color">
-                <label htmlFor="direita">Direita <br />
+                <label htmlFor="direita">Direita<br />
                     <input type="radio" id="direita" name="posicionamento" value="direita" onChange={() => props.changeHandler(6)} />
                 </label>
             </span>
             <span className="scale-value">
-                <label htmlFor="extrema-direita">Extrema-direita <br />
+                <label htmlFor="extrema-direita">Extrema-direita<br />
                     <input type="radio" id="extrema-direita" name="posicionamento" value="extrema-direita" onChange={() => props.changeHandler(7)} />
+                </label>
+            </span>
+            <span className="scale-value scale-value-color">
+                <label htmlFor="sem-escolha">Não sei / Ignorar<br />
+                    <input type="radio" id="sem-escolha" name="posicionamento" value="sem-escolha" onChange={() => props.changeHandler(8)} />
                 </label>
             </span>
         </div>
